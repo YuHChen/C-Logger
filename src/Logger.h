cd13752c6e35b56063db1fb7df80a9bf94a3ec8d
Yu Heng Chen <yuhchen21@gmail.com>
2015-09-12 12:26:39 -0400

    Revert "Changed namespace to class"
    This reverts commit 1603c0495fcd1053f026f798baa5e92f678d22fa.

--- src/Logger.h
+++ src/Logger.h
@@ -10,33 +10,29 @@
 #include <type_traits>
 #include <utility>
 
-class Logger {
-  typedef std::shared_ptr<std::ofstream> file_ptr;
-  
+namespace Logger {
+
   // ===== Declarations ===== //
- private:
+  ///// Types /////
+  typedef std::shared_ptr<std::ofstream> file_ptr;
+  enum class LogLevel;
+  enum class LogType;
   ///// Methods /////
+  void setLogLevel(LogLevel level);
+  void setIndentLevel(int level);
+  bool addLogFile(std::string filename);
+  bool removeLogFile(std::string filename);
   bool canLogMessage(LogLevel messageLogLevel);
   void beginLogging(void);
   void endLogging(void);
   void performLogging(std::string message, LogLevel level, int indent);
-  
- public:
-  ///// Types /////
-  enum class LogLevel;
-  enum class LogType;
-  ///// Methods /////
-  static void setLogLevel(LogLevel level);
-  static void setIndentLevel(int level);
-  static bool addLogFile(std::string filename);
-  static bool removeLogFile(std::string filename);
-  static void log(std::string message, LogType type, int indent);
-  static void log(std::string message, LogType type);
-  static void log(std::string message, LogLevel level, int indent);
-  static void log(std::string message, LogLevel level);
-  static void log(std::string message, int indent);
-  static void log(std::string message);
-  static void log(void);
+  void log(std::string message, LogType type, int indent);
+  void log(std::string message, LogType type);
+  void log(std::string message, LogLevel level, int indent);
+  void log(std::string message, LogLevel level);
+  void log(std::string message, int indent);
+  void log(std::string message);
+  void log(void);
 
   // ===== Definitions ===== //
   ///// Types /////
@@ -241,6 +237,6 @@ class Logger {
     
   }
   
-}; // end of class Logger
+} // end of namespace Logger
 
 #endif
